e75848380fb2a77b195d20dcee65f2f01f8ecfa7
rishabhporia02 <rishabhporia02@gmail.com>
2022-06-18 00:41:33 +0530

    Preparing for deployment

--- backend/middleware/authMiddleware.js
+++ backend/middleware/authMiddleware.js
@@ -7,15 +7,9 @@ const protect = asyncHandler(async(req, res, next) => {
 
     if (req.headers.authorization && req.headers.authorization.startsWith('Bearer')) {
         try {
-            // Get token from header
             token = req.headers.authorization.split(' ')[1]
-
-            // Verify token
             const decoded = jwt.verify(token, process.env.JWT_SECRET)
-
-            // Get user from the token
             req.user = await User.findById(decoded.id).select('-password')
-
             next()
         } catch (error) {
             console.log(error)
--- backend/server.js
+++ backend/server.js
@@ -1,3 +1,4 @@
+const path = require('path')
 const express = require('express')
 const connectDB = require('./config/db.js')
 const errorHandler = require('./middleware/errorMiddleware')
@@ -13,8 +14,18 @@ app.use(express.json())
 app.use(express.urlencoded({ extended: false }))
 app.use('/api/goals', require('./routes/goalRoutes.js'))
 app.use('/api/users', require('./routes/userRoutes.js'))
-app.use(errorHandler)
 
+if (process.env.NODE_ENV === 'production'){
+    app.use(express.static(path.join(__dirname, '../frontend/build')))
+    app.get('*', (req, res) => res.sendFile(
+        path.resolve(__dirname, '../', 'frontend', 'build', 'index.html')
+        )
+    )
+} else {
+    app.get('/', (req, res) => res.send('Please set to production.'))
+}
+
+app.use(errorHandler)
 app.listen(port, () => {
     console.log(`Listening on port ${port}...`.blue)
 })
